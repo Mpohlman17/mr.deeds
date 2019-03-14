7a87688bf2dd0d75286004bd6d47a6e054ac6ed1
Michael Pohlman <michael.pohlman1@gmail.com>
2019-03-14 00:12:47 -0400

    fixed contact route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,49 +3,49 @@
 // *********************************************************************************
 // Dependencies
 // =============================================================
-var path = require('path')
+var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 // var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // Routes
 // =============================================================
-module.exports = function (app) {
+module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to
   // loads homepage
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-  })
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
   // loads about page
-  app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/about.html'))
-  })
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+  });
 
   // loads contact page
-  app.get('/contact', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/about.html'))
-  })
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+  });
 
   // loads search page
-  app.get('/search', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/search.html'))
-  })
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
 
   // loads search page
-  app.get('/user/:user.id', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/user.html'))
-  })
+  app.get("/user/:user.id", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/user.html"));
+  });
 
   // loads 404 on any unconnect Url
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/404.html'))
-  })
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/404.html"));
+  });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   // Render 404 page for any unmatched routes
-  app.get('*', function (req, res) {
-    res.render('404')
-  })
-}
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
